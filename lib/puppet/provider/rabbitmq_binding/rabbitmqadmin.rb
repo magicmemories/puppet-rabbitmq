e55f44a543aaee3fcefb5e2fee6e0dbb1b66ef50
Adam Gardner <adam.gardner@magicmemories.com>
2020-04-14 16:18:33 -1000

    Include binding arguments in name calculation since they are part of the identity of the binding object

--- lib/puppet/provider/rabbitmq_binding/rabbitmqadmin.rb
+++ lib/puppet/provider/rabbitmq_binding/rabbitmqadmin.rb
@@ -41,7 +41,11 @@ Puppet::Type.type(:rabbitmq_binding).provide(:rabbitmqadmin, parent: Puppet::Pro
         else
           arguments = '{}'
         end
-        hashed_name = Digest::SHA256.hexdigest format('%s@%s@%s@%s%s', source_name, destination_name, vhost, routing_key, arguments)
+        if arguments == '{}'
+          hashed_name = Digest::SHA256.hexdigest format('%s@%s@%s@%s', source_name, destination_name, vhost, routing_key)
+        else
+          hashed_name = Digest::SHA256.hexdigest format('%s-%s-%s-%s-%s', source_name, destination_name, vhost, routing_key,arguments)
+        end
         next if source_name.empty?
         binding = {
           source: source_name,
